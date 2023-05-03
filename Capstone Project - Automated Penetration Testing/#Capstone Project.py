6a1d88ec2eabed50db432a572d6098a404eb1ea3
raayaw <ray.aw.jl@gmail.com>
2023-05-03 15:31:38 +0800

    Host Discovery edits

--- Capstone Project - Automated Penetration Testing/#Capstone Project.py
+++ Capstone Project - Automated Penetration Testing/#Capstone Project.py
@@ -86,12 +86,6 @@ def option_3():
     target = input("Enter an IP Address to scan: ")
     nm = nmap.PortScanner()
 
-    #nm.scan(hosts='192.168.1.128/24', arguments='-n -sP')
-    #hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
-
-    #for host, status in hosts_list:
-    #    print(host + ' ' + status)
-
     nm.scan(hosts=target, arguments='-n -sP')
     hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
 
