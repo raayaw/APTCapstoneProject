43dc441b8c0d071381fb0bde731ceb791bcc78f0
Jin Le Ray Aw <s10222533@connect.np.edu.sg>
2023-05-03 15:26:51 +0800

    Added Host Discovery using nmap

--- Capstone Project - Automated Penetration Testing/#Capstone Project.py
+++ Capstone Project - Automated Penetration Testing/#Capstone Project.py
@@ -83,6 +83,20 @@ def option_2():
 
 def option_3():
     print("This is option 3 function")
+    target = input("Enter an IP Address to scan: ")
+    nm = nmap.PortScanner()
+
+    #nm.scan(hosts='192.168.1.128/24', arguments='-n -sP')
+    #hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+
+    #for host, status in hosts_list:
+    #    print(host + ' ' + status)
+
+    nm.scan(hosts=target, arguments='-n -sP')
+    hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+
+    for host, status in hosts_list:
+        print(host + ' ' + status)
 
 def option_4():
     exit()
