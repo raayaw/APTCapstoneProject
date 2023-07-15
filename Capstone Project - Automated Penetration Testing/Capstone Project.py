555205c32254d5ffc1840537fd860f5637cbeea7
aioc08 <js2347272@gmail.com>
2023-07-15 11:24:59 +0800

    Added ZAP scan

--- Capstone Project - Automated Penetration Testing/Capstone Project.py
+++ Capstone Project - Automated Penetration Testing/Capstone Project.py
@@ -23,6 +23,10 @@ import builtwith #pip install builtwith
 from scapy.all import *
 import ldap3
 import pandas as pd
+from zapv2 import ZAPv2
+import time
+import subprocess
+from subprocess import Pope
 
 #FOR OPENVAS
 import subprocess
@@ -1733,6 +1737,48 @@ def crack_hash_generated():
     remove_rec_file = subprocess.call("rm /root/.john/john.rec", shell = True)
     start_listener = subprocess.call(['gnome-terminal', '-e', 'bash -c "john Responder/logs/{}; exec bash"'.format(hash_file)])
 
+def zap_scan():
+    zap_command = "/usr/share/zaproxy/zap.sh -config api.key=test"
+    subprocess.Popen(['gnome-terminal','--command',zap_command])
+    print("If a window opens saying 'Do you want to persist the ZAP Session?', please select 'No, I do not want to persist this session at this moment in time' and press start")
+    input("Press enter to continue once ZAP finishes booting up...")
+    time.sleep(5)
+    apikey = 'test'
+    zap = ZAPv2(apikey=apikey)
+    target = input('Enter the URL to attack (eg. http://example/.com): ')    
+    print('Accessing target:', target)
+    zap.urlopen(target)
+
+    # Spider the target URL
+    print('Spidering target URL...')
+    zap.spider.scan(target)
+
+    # Wait for the spidering to complete
+    while int(zap.spider.status()) < 100:
+        print('Spider progress:', zap.spider.status(), '%')
+        time.sleep(2)
+
+    # Start the active scan
+    print('Starting active scan...')
+    zap.ascan.scan(target)
+
+    # Wait for the active scan to complete
+    while int(zap.ascan.status()) < 100:
+        print('Active scan progress:', zap.ascan.status(), '%')
+        time.sleep(5)
+
+    # Generate the report
+    print('Generating report...')
+    report_html = zap.core.htmlreport()
+    report_xml = zap.core.xmlreport()
+
+    # Save the report to a file
+    with open('report.html', 'w') as f:
+        f.write(report_html)
+
+    with open('report.xml', 'w') as f:
+        f.write(report_xml)
+    
 
 project_menu()
 
